2c7b53eb9964247e16542a8bedf1da73ac30fe73
cristiandaw19 <55553799+cristiandaw19@users.noreply.github.com>
2019-10-03 18:45:36 +0200

    Update Adios.java

--- Adios.java
+++ Adios.java
@@ -5,10 +5,6 @@ public class Adios {
 		String amarillo = "\033[33m";
 		String vede = "\033[32";
 		String reset = "\033[0m";
-
-
-
-
 		System.out.println(amarillo + "Adios mundo!!!" + reset);
 		System.out.println(verde + "Pero...Volveré" + reset);
 	}
